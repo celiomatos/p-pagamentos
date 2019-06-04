1881da8100dd37ede0695a71ba1ee09a158372c5
Celio Matos <celio.matos@itbam.org.br>
2019-06-04 10:36:13 -0400

    third commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function run() {
+
+const dt = new Date();
+const ano = dt.getUTCFullYear();
+const mes = dt.getUTCMonth() > 8 ? (dt.getUTCMonth() + 1) : '0' + (dt.getUTCMonth() + 1).toString(); 
+const folder = dt.toISOString().split('T')[0];
+const basePath = 'http://www.transparenciafiscal.am.gov.br/transpprd/mnt/info/';
+
+if(!fs.existsSync(folder)){
+    fs.mkdirSync(folder);
+}
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.setViewport({width: 1280, height: 800});
+
+await page.goto(basePath + 'RelPagamentos.do?method=Pesquisar&anoexercicio=' + ano + '&mes=' + mes);
+const title = await page.title();
+await page.screenshot({ path: folder + '/' + title + '.png', fullPage: true });
+
+const data = await page.$$eval('#item tbody tr td a', tds => tds.map((td) => {
+  return td.href;
+}));
+
+for(let i = 0; i < data.length; i++){
+	const orgao = await data[i].substring(22,28);	
+    await page.goto(basePath + 'RelPagamentosOrgao.do?method=Pesquisar&anoexercicio=' + ano + '&mes=' + mes + '&counidadegestora=' + orgao);
+    await page.screenshot({ path: folder + '/' + orgao + '.png', fullPage: true });
+}
+
+await browser.close();
+}
+
+run();
